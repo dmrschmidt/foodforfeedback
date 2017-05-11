71b1732bdd4ffc740e67958a0f2fbf9fe6b65441
Dennis Schmidt <dschmidt@pivotal.io>
2017-05-11 14:00:08 +0200

    randomly pick from extended list of feedback prompts
    [#145255541]
    
    Signed-off-by: Max Brauer <mbrauer@pivotal.io>

--- spec/javascripts/FeedbackFeederSpec.js
+++ spec/javascripts/FeedbackFeederSpec.js
@@ -1,12 +1,34 @@
+function StubRandomizer(stubbedValue) {
+  this.stubbedValue = stubbedValue
+}
+StubRandomizer.prototype.constructor = StubRandomizer
+StubRandomizer.prototype.pickWithinRange = function(upperBound) {
+  return this.stubbedValue
+}
+
 describe("FeedbackFeeder", function() {
-  var feedbackFeeder
+  var prompts = ['foo', 'bar']
+  var randomizer1, randomizer2
 
-  it("returns a feedback prompt", function() {
-    feedbackFeeder = new FeedbackFeeder()
+  beforeEach(function() {
+    randomizer1 = new StubRandomizer(0)
+    randomizer2 = new StubRandomizer(1)
+    spyOn(randomizer1, 'pickWithinRange').and.callThrough()
+    spyOn(randomizer2, 'pickWithinRange').and.callThrough()
+  })
 
-    expect(feedbackFeeder.feedbackPrompt()).toEqual("We Ping Pong-paired really well today.")
+  it("returns a feedback prompt based on randomizer", function() {
+    var feedbackFeeder1 = new FeedbackFeeder(prompts, randomizer1)
+    var feedbackFeeder2 = new FeedbackFeeder(prompts, randomizer2)
+
+    expect(feedbackFeeder1.feedbackPrompt()).toEqual('foo')
+    expect(feedbackFeeder2.feedbackPrompt()).toEqual('bar')
+  })
+
+  it('restricts randomization to available prompts', function() {
+    var feedbackFeeder = new FeedbackFeeder(prompts, randomizer1)
+    feedbackFeeder.feedbackPrompt()
+
+    expect(randomizer1.pickWithinRange).toHaveBeenCalledWith(1);
   })
 })
-
-// Randomizer.random(0, 10).stub.to.return(7)
-// Randomizer.random(0, 10).stub.to.return(8)
--- src/FeedbackFeeder.js
+++ src/FeedbackFeeder.js
@@ -1,7 +1,18 @@
-function FeedbackFeeder() {
+function Randomizer() {
+}
+Randomizer.prototype.constructor = FeedbackFeeder
+
+Randomizer.prototype.pickWithinRange = function(upperBound) {
+  return Math.round(Math.random() * upperBound)
+}
+
+function FeedbackFeeder(prompts, randomizer) {
+  this.prompts = prompts
+  this.randomizer = randomizer
 }
 FeedbackFeeder.prototype.constructor = FeedbackFeeder
 
-FeedbackFeeder.prototype.feedbackPrompt = function () {
-  return "We Ping Pong-paired really well today."
+FeedbackFeeder.prototype.feedbackPrompt = function() {
+  var pick = this.randomizer.pickWithinRange(this.prompts.length - 1)
+  return this.prompts[pick]
 }
